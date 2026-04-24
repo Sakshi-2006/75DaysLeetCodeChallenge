dd8c165d0efa4cf6f62a5f04fc9ac9131ed91ca8
Sakshi Karwade <sakshikarwade2006@gmail.com>
2026-04-24 22:08:02 +0530

    Time: 26 ms (35.38%), Space: 50.6 MB (31.81%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,69 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isEnd = false;
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* node = root;
+
+        for (char ch : word) {
+            int index = ch - 'a';
+
+            if (node->children[index] == NULL) {
+                node->children[index] = new TrieNode();
+            }
+
+            node = node->children[index];
+        }
+
+        node->isEnd = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* node = root;
+
+        for (char ch : word) {
+            int index = ch - 'a';
+
+            if (node->children[index] == NULL) {
+                return false;
+            }
+
+            node = node->children[index];
+        }
+
+        return node->isEnd;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+
+        for (char ch : prefix) {
+            int index = ch - 'a';
+
+            if (node->children[index] == NULL) {
+                return false;
+            }
+
+            node = node->children[index];
+        }
+
+        return true;
+    }
+};
